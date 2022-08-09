cb8db6ab53040b962ce7d1bdaa744ae28fc5494a
Harish mahur <hmahur20@gmail.com>
2022-08-09 22:39:36 +0530

    modified:pop element

--- Main.java
+++ Main.java
@@ -2,16 +2,16 @@ package com.LinkedList;
 
 public class Main {
 
-	 
 	public static void main(String[] args) {
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        linkedList.push(70);
-        linkedList.push(56);
+		LinkedList<Integer> linkedList = new LinkedList<>();
+		linkedList.push(70);
+		linkedList.push(56);
 
-        linkedList.insert(30);
+		linkedList.insert(30);
 
-        linkedList.display();
+		System.out.println(linkedList.pop());
 
+		linkedList.display();
 
-    }
+	}
 }
